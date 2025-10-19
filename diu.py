87a967e2f71f5c0cb44478c65f60db768df2034c
diutuananh <minidagfie@gmail.com>
2025-10-19 23:53:28 +0700

    Update diu.py

--- diu.py
+++ diu.py
@@ -5,14 +5,14 @@ from typing import List, Tuple, Dict
 def stratified_split(labels: List[str], test_size: float, random_state: int = 42): #chia dữ liệu thành train và test
     random.seed(random_state) #dữ liệu sẽ giống nhau sau mỗi lần run
     by_cls: Dict[str, List[int]] = {} # index được gom vào 2 lớp
-    for i, y in enumerate(labels):  # ở đây t chọn dùng for chứ k dùng setdefault vì nếu xử lí messege có kí tự đặc biệt sẽ bị lỗi 
+    for i, y in enumerate(labels):  # ở đây t chọn dùng for chứ k dùng setdefault vì nếu xử lí message có kí tự đặc biệt sẽ bị lỗi 
         if y not in by_cls:
             by_cls[y] = []
         by_cls[y].append(i) 
     test_idx = [] 
     train_idx = [] #lưu các index
     for y, idx in by_cls.items(): #duyệt các lớp và index
-        random.shuffle(idx)
+        random.shuffle(idx) # xáo trộn idx để mỗi lần test là 1 idx khác nhau k bị lệ thuộc vào idx đầu, sau cùng chạy sẽ ra 1 kết quả
         k = max(1, int(len(idx)*test_size)) # mỗi lớp có ít nhất 1 mẫu test
         test_idx.extend(idx[:k]) # chuyển vào tập test
         train_idx.extend(idx[:k]) 
@@ -20,7 +20,7 @@ def stratified_split(labels: List[str], test_size: float, random_state: int = 42
     test_idx.sort()
     return train_idx, test_idx
 # ổn định thứ tự và từ điển
-def build_vocab_from_train(train_texts, k = 3000):
+def build_vocab_from_train(train_texts, k = 3000): #build = 3000 từ train
     freq = {} #lưu tần số xuất hiện các từ
     for t in train_texts: #duyệt từng câu
         words = tokenize(t) #không cần thiết lắm nhưng t muốn để vậy dễ hiểu hơn, để hiểu là tách thành các từ xong duyệt
@@ -30,6 +30,7 @@ def build_vocab_from_train(train_texts, k = 3000):
     k = min(k, len(items))
     vocab = [w for w, _ in items[:k]] # lấy nội dung, đánh dấu số lần xuất hiện
     return vocab
+
 
 
 
