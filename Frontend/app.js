c3dcd97aa66d7d82b75f771838fb8ba147021470
Ngọc Ánh <anhngoc392006@gmail.com>
2025-11-22 16:47:11 +0700

    Update app.js

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://sms-classification-project-group-1-3.onrender.com";
+const API_BASE_URL = "http://localhost:8000";
 
 // ==================== TAB MANAGEMENT FUNCTIONS ====================
 
